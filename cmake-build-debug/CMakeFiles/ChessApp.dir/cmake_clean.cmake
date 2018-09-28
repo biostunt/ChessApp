0e48410b0263554ff05579d8361ee568e6089d37
biostunt <nice.nikita99@mail.ru>
2018-09-28 13:34:42 +0300

    Added Interface class

---
+++ cmake-build-debug/CMakeFiles/ChessApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessApp.dir/main.cpp.o"
+  "ChessApp.pdb"
+  "ChessApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
