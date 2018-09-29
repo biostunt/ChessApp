6b0f7c01daf36daf8acb925c73634038e0255f88
NastasiaHarvard <43639372+NastasiaHarvard@users.noreply.github.com>
2018-09-29 15:00:41 +0300

    Update ChessField.h

--- ChessField.h
+++ ChessField.h
@@ -6,14 +6,25 @@
 #define CHESSAPP_CHESSFIELD_H
 
 
-
+#include <iostream>
+using namespace std;
 
 class ChessField{
 private:
     int size;
-    void initialize() {
+    const int M, N =8;
+    void initialize() {  
+	int **Field = new int* [N];  
+  for (int i = 0; i < N; i++) {       
+    Field [i]= new int [M]; 
+  }   
+  for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+           Field[i][j]= new Field(0);
 
-    }
+   	for (int i = 0; i < n; i++) {
+    delete [] Field[i];  
+  } }
 public:
     ChessField(int size){
         this->size = size;
@@ -28,7 +39,5 @@ public:
     }
 
 };
-
-
 
 #endif //CHESSAPP_CHESSFIELD_H
