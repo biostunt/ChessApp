964ae6caff0142a6496d2ed4505fe9fa67be46d0
NastasiaHarvard <43639372+NastasiaHarvard@users.noreply.github.com>
2018-09-30 18:56:44 +0300

    Update ChessField.h

--- ChessField.h
+++ ChessField.h
@@ -9,7 +9,7 @@
 
 class ChessField{
 private:
-    int size;
+    int size, id;
     Figure ***chessField;
     void initialize() {
         chessField = new Figure** [size];
@@ -18,6 +18,24 @@ private:
         for(int i = 0; i < size; i++)
             for(int j = 0 ; j < size; j++)
           chessField[i][j] = new Figure(0,i,j);
+  }
+  void Add(int x, int y){
+  	for(int i = 0; i < size; i++)
+            for(int j = 0 ; j < size; j++)
+  	switch(id){
+            case 1:
+                chessField[i][j] = new King(x,y);
+            case 2:
+                chessField[i][j] = new Queen(x,y);
+            case 3:
+                chessField[i][j] = new Elephant(x,y);
+            case 4:
+                chessField[i][j] = new Horse(x,y);
+            case 5:
+                chessField[i][j] = new Rook(x,y);
+            case 6:
+                chessField[i][j] = new Pawn(x,y);
+        }
   }
 public:
     ChessField(int size){
